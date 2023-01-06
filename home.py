5a0574c622b5b61b42c33c38c61741336f4bef2c
munnich <munnich@protonmail.com>
2023-01-06 17:18:13 +0100

    add simple markdown home post functin

---
+++ home.py
@@ -0,0 +1,17 @@
+from util import *
+import markdown as md
+
+def add_post(title, post):
+    db = connect()
+    db.home.insert_one({"title": title, "content": md.markdown(post)})
+    print("Done!")
+    return
+
+if __name__ == "__main__":
+    with open("post.md") as f:
+        lines = f.read().split("\n")
+        title = lines[0]
+        post = "\n".join(lines[1:])
+        add_post(title, post)
+        f.close()
+
