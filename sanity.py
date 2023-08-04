a03781f9a81af984485cfa24ad9b0e6c4bb43d61
Gilad Livne <g.livne@gmail.com>
2023-08-04 03:16:41 +0300

    sanity for FFA

--- sanity.py
+++ sanity.py
@@ -32,7 +32,7 @@ def sanity_check(data):
         if not game.split():
             continue
         players = game.split(", ")
-        mode = players[0]
+        mode = str(players[0]).lower()
         map_name = None
         if "$" in mode:
             temp = mode.split("$")
@@ -50,7 +50,7 @@ def sanity_check(data):
                         out += f"Unknown map or host detected in:\n{game}\n"
 
         # sanity check mode
-        if mode not in ["M", "E", "AA", "DO"]:
+        if mode not in GAME_MODES:
             out += f"Unknown mode {mode} detected in:\n{game}\n"
         
         # sanity check map name
@@ -65,27 +65,35 @@ def sanity_check(data):
                 identify_player(db, host_player)
             except:
                 out += f"Unknown host detected in:\n{game}\n"
-
-        num_players = int(players[1])
-        outcome = players[2]
-        outcome = int(outcome)
-        gamers = players[3:] # very epic
-
-        # check for missing players or commas
-        if len(gamers) != num_players * 2:
-            out += f"Formatting error or missing player found in:\n{game}\n"
-
-        # load player data
-        score = [0, 0]
-        kills = [0, 0]
-        deaths = [0, 0]
-        # team index
-        i = 0
+        
         num_delim = 3
-        if mode == "AA":
-            num_delim += 1
 
-        all_gamers = []
+        if mode not in FFA_MODES:
+            num_players = int(players[1])
+            outcome = players[2]
+            outcome = int(outcome)
+            gamers = players[3:] # very epic
+
+            # check for missing players or commas
+            if len(gamers) != num_players * 2:
+                out += f"Formatting error or missing player found in:\n{game}\n"
+
+            # load player data
+            score = [0, 0]
+            kills = [0, 0]
+            deaths = [0, 0]
+            # team index
+            i = 0
+            if mode == "AA":
+                num_delim += 1
+            all_gamers = []
+
+        else:
+            kills = 0
+            deaths = 0
+            score = 0
+            gamers = players[1:]
+        
         for j in range(len(gamers)):
             # check for $ delim count
             if gamers[j].count("$") != num_delim:
@@ -107,7 +115,10 @@ def sanity_check(data):
             # sum score
             if mode != "AA":
                 try:
-                    score[i] += int(player[1])
+                    if mode not in FFA_MODES:
+                        score[i] += int(player[1])
+                    else:
+                        score += int(player[1])
                 except ValueError:
                     out += f"Detected nonnumerical input for {gamers[j]} in:\n{game}\n"
             else:
@@ -121,28 +132,38 @@ def sanity_check(data):
                 out += f"Unusually high score detected in:\n{game.replace(player[1], f'**{player[1]}**')}\n" 
             # sum k/d
             try:
-                kills[i] += int(player[2])
+                if mode not in FFA_MODES:
+                    kills[i] += int(player[2])
+                else:
+                    kills += int(player[2])
             except ValueError:
                 out += f"Detected nonnumerical input for {gamers[j]} in:\n{game}\n"
             try:
-                deaths[i] += int(player[3])
+                if mode not in FFA_MODES:
+                    deaths[i] += int(player[3])
+                else:
+                    deaths += int(player[3])
             except ValueError:
                 out += f"Detected nonnumerical input for {gamers[j]} in:\n{game}\n"
 
-            # switch to next team
-            if j == num_players - 1:
-                i = 1
+            if mode not in FFA_MODES:
+                # switch to next team
+                if j == num_players - 1:
+                    i = 1
         
         # k/d check unfortunately doesn't work for console escort
         if mode != "E":
-            # sanity check k/d
-            for i in range(2):
-                # mildly retarded
-                if kills[i] != deaths[(i + 1) % 2]:
-                    out += f"Incorrect kill (team {i + 1})/death (team {(i + 1) % 3}) count detected in:\n{game}\n"
-        
+            if mode not in FFA_MODES:
+                # sanity check k/d
+                for i in range(2):
+                    # mildly retarded
+                    if kills[i] != deaths[(i + 1) % 2]:
+                        out += f"Incorrect kill (team {i + 1})/death (team {(i + 1) % 3}) count detected in:\n{game}\n"
+            elif kills != deaths:
+                out += f"Incorrect kill/death count detected in:\n{game}\n"
+
         # sanity check score and outcome
-        if mode != "DO":
+        if mode not in FFA_MODES and mode != "DO":
             if outcome > 0:
                 if max(score) != score[outcome - 1]:
                     out += f"Incorrect score/outcome detected in:\n{game}\n"
