e6781c25eac71179b7144075b181c35104b21087
glivne <g.livne@gmail.com>
2024-12-29 02:08:15 +0200

    allstar medals

--- add_badges.py
+++ add_badges.py
@@ -289,10 +289,12 @@ if __name__ == "__main__":
     add_badge("Gummy", "2024-12-28", "Rookie", "all", season, f"Season {season} Rookie of the Season")
 
 
-    #add_badge("", "2024-12-28", "Custom", "Domination", season, f"Season {season} Manhunt All-Star Match Champion", {"Discord": ":star2:", "HTML": "&#127775"})
-    #add_badge("", "2024-12-28", "Custom", "Domination", season, f"Season {season} Manhunt All-Star Match Champion", {"Discord": ":star2:", "HTML": "&#127775"})
-    #add_badge("", "2024-12-28", "Custom", "Domination", season, f"Season {season} Manhunt All-Star Match Champion", {"Discord": ":star2:", "HTML": "&#127775"})
-    #add_badge("", "2024-12-28", "Custom", "Domination", season, f"Season {season} Manhunt All-Star Match Champion", {"Discord": ":star2:", "HTML": "&#127775"})
+    add_badge("Edi", "2024-12-28", "Custom", "Domination", season, f"Season {season} Domination All-Star Match Champion", {"Discord": ":star2:", "HTML": "&#127775"})
+    add_badge("Lars", "2024-12-28", "Custom", "Domination", season, f"Season {season} Domination All-Star Match Champion", {"Discord": ":star2:", "HTML": "&#127775"})
+    add_badge("Xanthex", "2024-12-28", "Custom", "Domination", season, f"Season {season} Domination All-Star Match Champion", {"Discord": ":star2:", "HTML": "&#127775"})
+    add_badge("Lunaire.-", "2024-12-28", "Custom", "Domination", season, f"Season {season} Domination All-Star Match Champion", {"Discord": ":star2:", "HTML": "&#127775"})
+    add_badge("Cota", "2024-12-28", "Custom", "Domination", season, f"Season {season} Domination All-Star Match Champion", {"Discord": ":star2:", "HTML": "&#127775"})
+    add_badge("Arun", "2024-12-28", "Custom", "Domination", season, f"Season {season} Domination All-Star Match Champion", {"Discord": ":star2:", "HTML": "&#127775"})
 
     add_badge("Lunaire.-", "2024-03-26", "1st", "Deathmatch", season)
     add_badge("Arun", "2024-03-26", "2nd", "Deathmatch", season)
@@ -301,4 +303,4 @@ if __name__ == "__main__":
     add_badge("Lars", "2024-12-28", "All-Star", "Deathmatch", season, f"Season {season} Deathmatch All-Star")
     add_badge("Kapi", "2024-12-28", "All-Star", "Deathmatch", season, f"Season {season} Deathmatch All-Star")
     add_badge("Christian", "2024-12-28", "All-Star", "Deathmatch", season, f"Season {season} Deathmatch All-Star")
-    add_badge("Chuckie", "2024-12-28", "All-Star", "Deathmatch", season, f"Season {season} Deathmatch All-Star")
+    add_badge("CHUCKIE", "2024-12-28", "All-Star", "Deathmatch", season, f"Season {season} Deathmatch All-Star")
