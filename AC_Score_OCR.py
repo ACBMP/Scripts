6c5c8594bff4349b6729fb78bed3b6347b640c0f
munnich <munnich@protonmail.com>
2025-04-06 16:53:28 +0200

    ACR, AC4 OCR with OpenCV

--- AC_Score_OCR.py
+++ AC_Score_OCR.py
@@ -30,6 +30,7 @@ def OCR(screenshot: str, game: str, players: int, post_game: bool = False, ffa: 
         width = int(546 * scale)
         height = int(players * 26 * scale)
         binarize = [120, 145] # highlight, rest
+        threshold = 150
         img = img[top:top + height,left:left + width]
         common = {
                 "$S": "$5",
@@ -77,15 +78,18 @@ def OCR(screenshot: str, game: str, players: int, post_game: bool = False, ffa: 
                 "Crispi_$": "Crispi_",
                 }
     elif game.lower() == "acr":
-        scale = img.width / 1280
+        scale = img.shape[1] / 1280
         left = 518 * scale
         top = [194 * scale, 360 * scale]
         right=352 * scale
         bottom = [430 * scale, 264 * scale]
         binarize = [150, 115]
-        t = img.std.Crop(left=left, top=top[0], right=right, bottom=bottom[0])
-        b = img.std.Crop(left=left, top=top[1], right=right, bottom=bottom[1])
-        img = core.std.StackVertical([t, b])
+        threshold = 200
+        left = int(left)
+        top = [int(i) for i in top]
+        width = int(img.shape[1] - left - right)
+        height = int(img.shape[0] - top[0] - bottom[0])
+        img = cv2.vconcat([img[top[i]:top[i] + height, left:left + width] for i in range(2)])
         blue_v = [0, 255]
         common = {
                 "piesiol": "piesio1",
@@ -111,11 +115,11 @@ def OCR(screenshot: str, game: str, players: int, post_game: bool = False, ffa: 
                 "iQueazo4": "iQueazo24"
                 }
     elif game.lower() == "ac4":
-        scale = img.width / 1920
-        scale_y = img.height / 1080
+        scale = img.shape[1] / 1920
+        scale_y = img.shape[0] / 1080
         blue_v = [0, 255]
+        threshold = 150
         if not ffa:
-            # although supported post-game screenshots tend to perform worse
             if not post_game:
                 left = 650 * scale
                 top = [660 * scale_y, 890 * scale_y]
@@ -123,21 +127,31 @@ def OCR(screenshot: str, game: str, players: int, post_game: bool = False, ffa: 
                 bottom = [267 * scale_y, 39 * scale_y]
                 binarize = [140, 90]
             else:
+                # get rid of abstergo nonsense
+                for h in range(img.shape[0]):
+                    for w in range(img.shape[1]):
+                        if orig[h, w, 0] < 60 and orig[h, w, 2] > 100:
+                            img[h, w] = 255
                 left = 906 * scale
                 top = [526 * scale_y, 755 * scale_y]
                 right = 196 * scale
                 bottom = [402 * scale_y, 174 * scale_y]
-                binarize = [150, 130]
-            t = img.std.Crop(left=left, top=top[0], right=right, bottom=bottom[0])
-            b = img.std.Crop(left=left, top=top[1], right=right, bottom=bottom[1])
-            img = core.std.StackVertical([t, b])
+                binarize = [140, 130]
+            left = int(left)
+            top = [int(i) for i in top]
+            width = int(img.shape[1] - left - right)
+            height = int(img.shape[0] - top[0] - bottom[0])
+            img = cv2.vconcat([img[top[i]:top[i] + height, left:left + width] for i in range(2)])
+            img = cv2.hconcat([img[:, :360], img[:, 375:560], img[:, 575:710], img[:, 725:]])
         else:
-            left = 650 * scale
-            top = 682 * scale_y
-            right = 486 * scale
-            bottom = 90 * scale_y
+            left = int(650 * scale)
+            top = int(682 * scale_y)
+            right = int(486 * scale)
+            bottom = int(90 * scale_y)
             binarize = [140, 90]
-            img = img.std.Crop(left=left, top=top, right=right, bottom=bottom)
+            width = int(img.shape[1] - left - right)
+            height = int(img.shape[0] - top - bottom)
+            img = img[top:top + height, left:left + width]
             blue_v = [255, 0]
         common = {
                 "She$Who": "She_Who",
@@ -165,7 +179,8 @@ def OCR(screenshot: str, game: str, players: int, post_game: bool = False, ffa: 
     else:
         return OCR(screenshot, "acb", players)
 
-
+    # cv2.imshow("", img)
+    # cv2.waitKey(0)
     img = cv2.resize(img, [i * 3 for i in img.shape[:2]][::-1], interpolation=cv2.INTER_NEAREST)
 
     # split players
@@ -176,12 +191,14 @@ def OCR(screenshot: str, game: str, players: int, post_game: bool = False, ffa: 
         img_arr.append(img[top:top + img.shape[0] // players, :])
 
     for i in range(len(img_arr)):
-        if np.median(img_arr[i]) < 150:
+        if np.median(img_arr[i]) < threshold:
             _, img_arr[i] = cv2.threshold(img_arr[i], binarize[0], 255, cv2.THRESH_BINARY_INV)
         else:
             _, img_arr[i] = cv2.threshold(img_arr[i], binarize[1], 255, cv2.THRESH_BINARY)
+        # cv2.imshow("", img_arr[i])
+        # cv2.waitKey(0)
 
-    whitelist = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.-_ "
+    whitelist = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.-_ []"
     for i in img_arr:
         result = pytesseract.image_to_string(i, lang="eng", config=f"--psm 7 -c tessedit_char_whitelist={whitelist}")
         result = result.replace("'", "").replace(" ", "$").replace("\\n", ", ")
@@ -225,4 +242,9 @@ def correct_score(match: str, correction: int, team: int):
 
 
 if __name__ == "__main__":
-    OCR("./escort1.png", "acb", 6)
+    OCR("./manhunt1.png", "acb", 6)
+    OCR("./escort1.png", "acb", 4)
+    OCR("./domi1.jpg", "ac4", 8, post_game=True)
+    OCR("./domi3.png", "ac4", 8, post_game=True)
+    OCR("./aa1.png", "acr", 8)
+    OCR("./domi2.jpg", "ac4", 8, post_game=False)
