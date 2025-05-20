cdd255a2879b7ca82b7dc0a380f3bf7494a0cb5f
munnich <munnich@protonmail.com>
2025-05-20 11:26:25 +0200

    update queue bot for new vresions

--- queue_bot.py
+++ queue_bot.py
@@ -16,13 +16,12 @@ import re
 db = util.connect()
 
 scheduler = AsyncIOScheduler()
-scheduler.start()
 
 intents = discord.Intents.default()
 intents.members = True
-#intents.message_content = True
+intents.message_content = True
 
-client = commands.Bot(command_prefix="an", intents=intents)
+client = discord.Client(intents=intents)
 
 modes_list = ['e', 'mh', 'aar', 'aad', 'do', 'dm', 'asb']
 modes_dict = {
@@ -40,6 +39,7 @@ modes_dict = {
 @client.event
 async def on_ready():
     await update_presence()
+    scheduler.start()
     print("Starting.")
 
 
